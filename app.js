eceb963474bc8b041da28dd0ca0a5bb6f5b47c7d
Scott Ganyo <scott@ganyo.com>
2014-08-20 13:04:11 -0700

    switch from json to yaml, require some magic

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 'use strict';
 
-
-/******* Swagger *******/
-
-var swaggerTools = require('swagger-tools').middleware.v2_0;
-var swaggerObject = require('./api/swagger/swagger.json');
-var volosSwagger = require('volos-swagger');
-
-// todo: make useStubs configurable via a127
-var swaggerRouterConfig = { useStubs: false, controllers: './api/controllers' }
-
-
-/**** Express ****/
+var a127 = require('a127-magic');
 
 var express = require('express');
 
 var app = express();
 
-app.use(swaggerTools.swaggerMetadata(swaggerObject)); // must be first of swagger middleware
-app.use(swaggerTools.swaggerValidator()); // include immediately after metadata
-
-app.use(volosSwagger()); // include after validation and before router
-
-app.use(swaggerTools.swaggerRouter(swaggerRouterConfig)); // route requests to your controllers
+app.use(a127.middleware());
 
 app.listen(process.env.PORT || 10010);
 
